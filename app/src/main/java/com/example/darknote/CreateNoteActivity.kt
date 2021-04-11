bf2e0b6b2e1a2b2bbabbcc5d710b6656c0f3ac42
Harsh <patelharsh0603@gmail.com>
2021-04-11 23:12:19 +0530

    Created Basic layout of activity_create_note.xml and added relevant resources for the same.

---
+++ app/src/main/java/com/example/darknote/CreateNoteActivity.kt
@@ -0,0 +1,11 @@
+package com.example.darknote
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CreateNoteActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_note)
+    }
+}
